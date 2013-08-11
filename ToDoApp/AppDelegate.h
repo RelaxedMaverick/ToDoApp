caf7d68d0714ec4765a7dee9f9f02ee9bb23a4b1
Rohit Dialani <rohitd@yahoo-inc.com>
2013-08-11 15:16:33 +0530

    Adding base project

---
+++ ToDoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by Rohit Dialani on 11/08/13.
+//  Copyright (c) 2013 Rohit Dialani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoAppTests/ToDoAppTests.h
@@ -0,0 +1,13 @@
+//
+//  ToDoAppTests.h
+//  ToDoAppTests
+//
+//  Created by Rohit Dialani on 11/08/13.
+//  Copyright (c) 2013 Rohit Dialani. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ToDoAppTests : SenTestCase
+
+@end
